c3440d76081fec90a46aeef5263ce9b8eab6cd41
piartz <45662294+piartz@users.noreply.github.com>
2022-09-26 16:19:09 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,3 @@
-resource "aws_instance" "hello-world" {
-  ami = "ami-ef92b08a"
-  instance_type = "t2.micro"
-
-  provisioner "local-exec" {
-    command = <<EOH
-      sudo yum -y update
-      sudo yum install -y python3.6
-      python3 simple-hello-world.py
-    EOH
-  }
-
-  user_data = "${file("${path.module}/simple-hello-world.py")}"
-
-  tags {
-    Name = "my-aws-terraform-hello-world"
-  }
-}
-
-output "my-public-ip"{
-       value= aws_instance.instance.public_ip
-}
-
 resource "aws_security_group" "allow-tcp" {
   name = "my-aws-terraform-hello-world"
   ingress {
@@ -29,4 +6,29 @@ resource "aws_security_group" "allow-tcp" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "hello-world" {
+  ami = "ami-ef92b08a"
+  instance_type = "t2.micro"
+
+  provisioner "file" {
+    source = "webservice"
+    destination = "~"
+  }
+  provisioner "local-exec" {
+    command = <<EOH
+      sudo yum -y update
+      sudo yum install -y python3.6
+      python3 simple-hello-world.py
+    EOH
+  }
+
+  tags {
+    Name = "my-aws-terraform-hello-world"
+  }
+}
+
+output "my-public-ip"{
+       value= aws_instance.instance.public_ip
 }
